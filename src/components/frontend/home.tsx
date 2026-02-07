0a7a0d60a7de835e5eae5a89ccbbd725808fb99a
mrashed21 <rashedjaman768@gmail.com>
2026-02-07 11:37:24 +0600

    add shadcn

---
+++ src/components/frontend/home.tsx
@@ -0,0 +1,13 @@
+"use client";
+import { Button } from "../ui/button";
+
+const Home = () => {
+  return (
+    <section>
+      {" "}
+      <Button>Button</Button>
+    </section>
+  );
+};
+
+export default Home;
